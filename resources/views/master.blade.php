8eb03dfa4576e8ed465315ada420c34f9b07c678
muhammad shahid <maalik9272@gmail.com>
2021-12-25 12:30:03 +0500

    dealzbook.ae commit 8909809

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -83,7 +83,15 @@
     </script>
   
 
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-CFTDVKMRCN"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
+  gtag('config', 'G-CFTDVKMRCN');
+</script>
 
 
     
